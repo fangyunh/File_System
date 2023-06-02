f32289f70b3daad0afd4af28c00ade8a2ea0a21d
FelixYH <52960140+fangyunh@users.noreply.github.com>
2023-06-01 22:09:02 -0700

    Add files via upload

--- libfs/fs.c
+++ libfs/fs.c
@@ -387,6 +387,7 @@ int fs_write(int fd, void *buf, size_t count)
 
     size_t remaining = count;
     uint write_size = 0;
+    size_t buf_pos = 0;
 
     while (remaining > 0) {
         int data_blk_idx = get_data_blk_idx(fd);
@@ -395,7 +396,6 @@ int fs_write(int fd, void *buf, size_t count)
         size_t offset_in_blk = cur_offset % BLOCK_SIZE;
         size_t cost = 0;
         size_t free_idx = 0;
-        size_t buf_pos = 0;
 
         if (BLOCK_SIZE - offset_in_blk > remaining) {
             cost = remaining;
@@ -408,7 +408,7 @@ int fs_write(int fd, void *buf, size_t count)
             return -1;
         }
 
-        memcpy(bounce + cur_offset % BLOCK_SIZE, (char *)(buf + buf_pos), cost+1);
+        memcpy(bounce + cur_offset % BLOCK_SIZE, (char *)(buf + buf_pos), cost);
         block_write(data_blk_idx, (void *)bounce);
         buf_pos += cost;
         remaining -= cost;
@@ -443,10 +443,55 @@ int fs_write(int fd, void *buf, size_t count)
 int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+    if (!is_mount) {
+        return -1;
+    }
 
-    printf("File descriptor: %d\n", fd);
-    printf("Buffer address: %p\n", buf);
-    printf("Count: %zu\n", count);
-    return 0;
+    if (fd >= FS_OPEN_MAX_COUNT || fd < 0) {
+        return -1;
+    }
+
+    if (opened_fd[fd].seat == 0) {
+        return -1;
+    }
+
+    if (buf == NULL) {
+        return -1;
+    }
+
+    size_t remaining = count;
+    uint read_size = 0;
+    size_t buf_pos = 0;
+
+    while (remaining > 0) {
+        int data_blk_idx = get_data_blk_idx(fd);
+        char *bounce = (char *) calloc(BLOCK_SIZE, sizeof(char));
+        int cur_offset = opened_fd[fd].offset;
+        size_t offset_in_blk = cur_offset % BLOCK_SIZE;
+        size_t cost = 0;
+
+
+        if (BLOCK_SIZE - offset_in_blk > remaining) {
+            cost = remaining;
+        } else {
+            cost = BLOCK_SIZE - offset_in_blk;
+        }
+
+        if (block_read(data_blk_idx, (void *)bounce) == -1) {
+            free(bounce);
+            return -1;
+        }
+
+        memcpy((char *)(buf + buf_pos), bounce + cur_offset % BLOCK_SIZE, cost);
+        buf_pos += cost;
+        remaining -= cost;
+        read_size += cost;
+        cur_offset += cost;
+
+        fs_lseek(fd, cur_offset);
+        free(bounce);
+    }
+
+    return read_size;
 }
 
